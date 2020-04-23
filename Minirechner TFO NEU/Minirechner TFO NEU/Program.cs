7118007b555bf4a2e971a721860ec9949bfccf8b
Schwellen001 <02schjul@rgto-me.it>
2020-04-23 11:19:05 +0200

    Main Erstellen

--- Minirechner TFO NEU/Minirechner TFO NEU/Program.cs
+++ Minirechner TFO NEU/Minirechner TFO NEU/Program.cs
@@ -9,6 +9,18 @@ namespace Minirechner_TFO_NEU
     {
         static void Main(string[] args)
         {
-        }
+
+            Console.WriteLine("Minirechner");
+
+            Console.WriteLine("Zahl1: ");
+            int Zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Zahl2: ");
+            int Zahl2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+        } // Ende
     }
 }
