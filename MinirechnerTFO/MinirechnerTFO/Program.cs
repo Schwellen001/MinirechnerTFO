16eaab3f0e2c39343cfbf9b96c2db3b5a25679c0
Schwellen001 <02schjul@rgto-me.it>
2020-04-23 10:50:16 +0200

    Erstellung des Titels und Zahlen eingeben

--- MinirechnerTFO/MinirechnerTFO/Program.cs
+++ MinirechnerTFO/MinirechnerTFO/Program.cs
@@ -11,10 +11,18 @@ namespace MinirechnerTFO
         {
             // Main
 
+            Console.WriteLine("Minirechner");
+
+            Console.WriteLine();
+
+            Console.Write("Geben Sie eine Zahl ein: ");
+            int Zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Geben Sie eine weitere Zahl ein: ");
+            int Zahl2 = Convert.ToInt32(Console.ReadLine());
 
 
-
-
+            Console.ReadKey(); 
 
 
         } // Ende des Programmes
